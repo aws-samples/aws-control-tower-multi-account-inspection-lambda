1f8a4b7e2a6c5097337f68f7c5dae56de8a950b1
Mangalore <mangalos@3c22fb7b33df.ant.amazon.com>
2022-10-22 19:34:51 -0500

    Changed print statements to logging and fixed looping error

--- dangling-dns-records/src/app.py
+++ dangling-dns-records/src/app.py
@@ -1,6 +1,9 @@
 import os
 import boto3
+import botocore
 import datetime
+import logging
+logger = logging.getLogger()
 
 bucketName = os.environ.get('DestinationBucketName')
 
@@ -16,6 +19,9 @@ def lambda_handler(event, context):
         try:
             # Skip Management account
             if ("::"+account["Id"]+":" not in account["Arn"]) and (account["Status"] == 'ACTIVE'):
+
+                logger.info("Checking DNS for account {0}({1} - {2})".format(
+                    account["Id"], account["Name"], account["Status"]))
 
                 sts_connection = boto3.client('sts')
                 # Assume role in target account
@@ -47,12 +53,12 @@ def lambda_handler(event, context):
                 hosted_zones = rt53_connection.list_hosted_zones()
                 for hosted_zone in [hosted_zone for hosted_zone in hosted_zones['HostedZones'] if not hosted_zone["Config"]["PrivateZone"]]:
                     record_sets = rt53_connection.list_resource_record_sets(
-                        hosted_zone_id=hosted_zone["Id"]
+                        HostedZoneId=hosted_zone["Id"]
                     )
-                    for record_set in record_sets['ResourceRecordSets']:
-                        for record in (record for record in record_set['ResourceRecords'] if (record_set['Type'] == 'A' and 'ResourceRecords' not in record_set)):
+                    for record_set in [record_set for record_set in record_sets['ResourceRecordSets'] if record_set['Type'] == 'A' and 'ResourceRecords' in record_set]:
+                        for record in record_set['ResourceRecords']:
                             found = False
-                            #  Iterate over regions until its found (success) or exhausts all regions
+                            #  Iterate over regions until the IP is found (success) or you exhaust all regions
                             for region in ec2_client.describe_regions()['Regions']:
                                 try:
                                     ec2_client = boto3.client(
@@ -67,25 +73,25 @@ def lambda_handler(event, context):
                                     )
                                     found = True
                                     break
-                                except Exception as ec2error:
-                                    # The IP address does not exist in any region
+                                except botocore.exceptions.ClientError as ec2error:
+                                    # Exception denotes IP not found; continue checking
                                     continue
-                        if not found:
-                            audit_result += ','.join(
-                                [account["Id"], hosted_zone["Id"], record_set["Name"], record['Value'], '\n'])
+                            if not found:
+                                audit_result += ','.join(
+                                    [account["Id"], hosted_zone["Id"], record_set["Name"], record['Value'], '\n'])
             else:
-                print("Skipping : ----- ",
-                      account["Id"], "|", account["Name"], "|", account["Status"])
-
-        except Exception as e:
-            print("Exception : ##### ", account["Id"], e)
+                logger.warning("Skipping check for account {0}({1} - {2})".format(
+                    account["Id"], account["Name"], account["Status"]))
+        except (botocore.exceptions.ParamValidationError, botocore.exceptions.ClientError) as e:
+            logger.error(
+                "Exception in DNS check for account {0}: {1} ".format(account["Id"], e))
             continue
 
     if audit_result != "":
         s3 = boto3.resource('s3')
         today = datetime.date.today()
         objectName = today.strftime("%Y/%m/%d/auditreport")
-        audit_report = "Account#, Hosted Zone ID, Record Set Name, IP Address, IP Owned \n" + auditresult
+        audit_report = "Account#, Hosted Zone ID, Record Set Name, IP Address, IP Owned \n" + audit_result
         s3.Bucket(bucketName).put_object(Key=objectName, Body=audit_report)
 
         return {
